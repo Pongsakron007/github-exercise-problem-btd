a8662c401d74f47a31d40dc3cf3df73b7a4e48ee
Pongsakron007 <109970650+Pongsakron007@users.noreply.github.com>
2022-08-08 19:39:30 +0700

    Add files via upload
    เรียกชื่อฉันสิ

---
+++ call_myname_1star.py
@@ -0,0 +1,23 @@
+'''
+คำอธิบาย
+พูดชื่อฉันสิ
+
+รูปเเบบ Input
+ชื่อเล่น(s)
+
+รูปเเบบ Output
+Hello! ชื่อเล่น(s)
+
+ข้อจำกัด
+3 <= len(s) <= 20
+
+Input:
+Jack
+
+Output:
+Hello! Jack
+
+'''
+
+
+print("Hello! "+input())
