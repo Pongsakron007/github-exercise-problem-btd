0e1587600623f67f2cd9d2914847fb256f349697
Pongsakron007 <109970650+Pongsakron007@users.noreply.github.com>
2022-08-18 19:17:58 +0700

    Add files via upload
    ถอดสแครูท1ดาว

---
+++ squaroot_1star.py
@@ -0,0 +1,27 @@
+'''
+คำอธิบาย
+เราจะให้คุณถอด
+square root 
+ของตัวเลขของที่ input เข้าไป
+
+รูปเเบบ Input
+เป็น Integer (จำนวนเต็ม)
+
+รูปเเบบ Output
+คำตอบของ
+square root ของ input
+
+ตัวอย่างที่1
+Input:
+4
+
+Output:
+2
+'''
+
+import math
+num = int(input())
+
+result = math.sqrt(num)
+
+print(int(result))
