84227907dc7d1e6c51d21064834ce427051366b7
Pongsakron007 <109970650+Pongsakron007@users.noreply.github.com>
2023-06-16 21:50:05 +0700

    even_odd_1star
    even_odd_1star

---
+++ even_odd_1star.py
@@ -0,0 +1,23 @@
+'''
+ตัวอย่างที่1
+Input:
+1
+
+Output:
+odd
+
+ตัวอย่างที่2
+Input:
+2
+
+Output:
+even
+
+ตัวอย่างที่3
+Input:
+3
+
+Output:
+odd
+'''
+print("even" if int(input()) % 2 == 0 else "odd")
